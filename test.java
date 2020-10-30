6ebeceb11bfc40700c0586573f2698e0eadb8379
mbaihaqih1761 <73376034+mbaihaqih1761@users.noreply.github.com>
2020-10-31 03:02:08 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test {
+
+	
+	public static void main(String[] args) {
+
+	String nama = "Baihaqi";
+	String status = "Pengangguran";
+	
+	int umur = 19;
+	 int saldoRekA = 17_000;
+	  int saldoRekB = 601_000;
+	   int totalSaldo=saldoRekA+saldoRekB;
+		
+		System.out.println("Nama : "+nama+" - Status : "+status+" - Umur : "+umur+"(tahun) -  Tabungan Rp."+(saldoRekA+saldoRekB));
+	
+	}
+}
